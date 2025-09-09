7c1957a372178a5d163360f6895bcc1e2e9da16d
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-09-09 19:11:48 +0200

    add

--- app.go
+++ app.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"opechains.shop/chunklizer/v2/routes"
+)
 
+func main() {
+	app := fiber.New()
+	routes.HandleRoutes(app)
+	app.Listen(":3000")
 }
